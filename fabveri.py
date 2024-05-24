802ecd3005917db82e577f2bfba35f635f8c8e26
yagmire <150776519+yagmire@users.noreply.github.com>
2024-05-24 00:40:04 -0700

    more windows support

--- fabveri.py
+++ fabveri.py
@@ -1,14 +1,22 @@
-import os, requests, tarfile, shutil, json, colorama, platform
+import os, requests, tarfile, shutil, json, colorama, platform, zipfile
 from tqdm import tqdm
-
+from sys import exit
 print("Fabveri\n")
+
+# OS Detect
+if platform.system() != "Linux":
+    if platform.system() != "Windows":
+        exit()
 
 # Get latest links
 if os.path.isfile("links.json"):
     os.remove("links.json")
 links = json.loads(requests.get("https://raw.githubusercontent.com/yagmire/fabveri/main/links.json").text)
 fabricURL = links["fabric"]
-javaURL = links["java"]
+if platform.system() == "Linux":
+    javaURL = links["java-linux"]
+elif platform.system() == "Windows":
+    javaURL = links["java-windows"]
 print("Updated links.")
 
 # Getting fabric installer
@@ -27,26 +35,37 @@ else:
     print("Successfully downloaded fabric installer.")
 
 # Getting java
-if os.path.isdir("java"):
-    print("Java already exists.")
-else:
-    r = requests.get(javaURL, stream=True)
-    total_size = int(r.headers.get('content-length', 0))
-    block_size = 1024  # 1 Kibibyte
-    progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True)
-    with open('java.tar.gz', 'wb') as f:
-        for data in r.iter_content(block_size):
-            progress_bar.update(len(data))
-            f.write(data)
-    progress_bar.close()
-    java = tarfile.open('java.tar.gz') 
-    java.extractall('./java')
-    java.close()
-    print("Successfully downloaded java.")
+if platform.system() == "Linux":
+    if os.path.isdir("java"):
+        print("Java already exists.")
+    else:
+        r = requests.get(javaURL, stream=True)
+        total_size = int(r.headers.get('content-length', 0))
+        block_size = 1024  # 1 Kibibyte
+        progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True)
+        with open('java.tar.gz', 'wb') as f:
+            for data in r.iter_content(block_size):
+                progress_bar.update(len(data))
+                f.write(data)
+        progress_bar.close()
+        java = tarfile.open('java.tar.gz') 
+        java.extractall('./java')
+        java.close()
+        print("Successfully downloaded java.")
 
-req_snapshot = input(f"{colorama.Fore.BLUE}Use a snapshot version? (y or n)\n")
+elif platform.system() == "Windows":
+    if os.path.isdir("java"):
+        print("Java already exists.")
+    else:
+        r = requests.get(javaURL, stream=True)
+        with zipfile.ZipFile("java.zip", 'r') as zip_ref:
+            zip_ref.extractall("java")
+        print("Successfully downloaded java.")
 
-req_ver = input(f"What version of Minecraft?{colorama.Fore.WHITE}\n")
+
+req_snapshot = input(f"{colorama.Fore.BLUE}Use a snapshot version? (y or n)\n{colorama.Fore.WHITE}")
+
+req_ver = input(f"{colorama.Fore.BLUE}What version of Minecraft?{colorama.Fore.WHITE}\n")
 
 def ifSnapshot():
     if req_snapshot.lower() == "y":
@@ -55,17 +74,35 @@ def ifSnapshot():
         return ""
     
 def cleanup():
-    os.remove("fabricinstaller.jar")
-    os.remove("java.tar.gz")
-    shutil.rmtree("java")
+    # Fabric installer
+    try:
+        os.remove("fabricinstaller.jar")
+    except Exception as e:
+        print(f"\n{colorama.Fore.RED}There was an error deleting the fabric installer.\n{e}{colorama.Fore.WHITE}\n")
+    
+    # Java archive
+    try:
+        os.remove("java.tar.gz")
+    except Exception as e:
+        print(f"\n{colorama.Fore.RED}There was an error deleteing the java archive.\n{e}{colorama.Fore.WHITE}\n")
+
+    # Java Folder
+    try:
+        shutil.rmtree("java")
+    except Exception as e:
+        print(f"\n{colorama.Fore.RED}There was an error deleting the java folder.\n{e}{colorama.Fore.WHITE}\n")
 
 print(f"Fabric installer: {os.path.isfile('fabricinstaller.jar')}")
 print(f"Java: {os.path.isdir('./java')}")
 
-if platform.platform() == "Linux":
-    os.system(f"./java/jdk-17.0.9/bin/java -jar fabricinstaller.jar client {ifSnapshot()} -mcversion {req_ver}")
-elif platform.platform() == "Windows":
-    os.system(f"java/jdk-17.0.9/bin/java -jar fabricinstaller.jar client {ifSnapshot()} -mcversion {req_ver}")
+if platform.system() == "Linux":
+    operation = f"./java/jdk-17.0.9/bin/java -jar fabricinstaller.jar client {ifSnapshot()} -mcversion {req_ver}"
+    print(f"Running system command: {operation}")
+    os.system(operation)
+elif platform.system() == "Windows":
+    operation = f"java/jdk-17.0.9/bin/java.exe -jar fabricinstaller.jar client {ifSnapshot()} -mcversion {req_ver}"
+    print(f"Running system command: {operation}")
+    os.system(operation)
 
 cleanup()
 print("Cleaned up.")
